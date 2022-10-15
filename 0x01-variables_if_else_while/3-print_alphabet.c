14a03587f475210ca77af77ef609e1204f83d006
Angelo <onyedikaufelle@gmail.com>
2022-10-14 20:26:47 -0700

    pls

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>                                /**                                                * main - entry point
+ * Return: Always 0                                */                                               int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	whil
+		(d <= 'z') {                                              putchar(c);
+                        c++;
+                }
+        putchar('\n');
+        return (0);
+}
