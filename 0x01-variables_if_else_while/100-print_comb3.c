c6eea6eb1805942909fde0bdc845e3325b0c0510
Angelo <onyedikaufelle@gmail.com>
2022-10-17 20:16:41 -0700

    advance

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		for (a = '0'; a <= '9'; a++)
+		{
+			if (!((a == b) || (b > a)))
+			{
+				putchar(b);
+				putchar(a);
+				if (!(a == '9' && b == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
