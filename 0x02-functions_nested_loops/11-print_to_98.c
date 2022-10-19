00188a4efcd1b866923db55986e294b82880c5df
Angelo <onyedikaufelle@gmail.com>
2022-10-18 21:12:51 -0700

    mandatory task done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_to_98 - print entry point
@@ -12,12 +12,12 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
-			printf("%d\n", n);
+		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
-			printf("%d\n", n);
+		printf("%d\n", n);
 	}
 }
