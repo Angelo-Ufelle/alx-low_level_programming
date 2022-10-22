37db774b52c207146667786425b4fc51f28043e7
Angelo <onyedikaufelle@gmail.com>
2022-10-21 21:52:58 -0700

    finish

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int a, tri;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (tri = size - a; tri > 0; tri--)
+				_putchar(' ');
+
+			for (tri = 0; tri < a; tri++)
+				_putchar('#');
+
+			if (a == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
